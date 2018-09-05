32e6ab2ba763d931aa44451fed8830388130e44e
nguyenthanhson <son.thanh.97@gmail.com>
2018-09-05 10:45:17 +0700

    merge timestamp

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ let bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get("/api", (req, res) => {
+app.get("/api/timestamp/:dateString?", (req, res) => {
+  let dateString = req.params.dateString;
+  let date = dateString ? new Date(dateString) : new Date();
+  let unix = date.getTime();
+  let utc = date.toUTCString();
+  if (unix) res.json({
+    unix: unix,
+    utc: utc
+  })
+  else res.json({error: utc});
+})
+
+app.get("/api/header", (req, res) => {
   let ip = req.get('x-forwarded-for') || req.connection.remoteAddress;
   let lang = req.get("accept-language");
   let sys = req.get("user-agent");
