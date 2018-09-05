ea4abb9e0323abd89b2b2ab7e6ed04e3dd184cdf
nguyenthanhson <son.thanh.97@gmail.com>
2018-09-06 00:57:22 +0700

    finish shorturl

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 let express = require("express");
 let app = express();
 let bodyParser = require("body-parser");
+let dns = require("dns");
+let shortened = {0: "https://freecodecamp.org"};
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.get("/api/timestamp/:dateString?", (req, res) => {
   let dateString = req.params.dateString;
@@ -25,6 +28,30 @@ app.get("/api/header", (req, res) => {
     language: lang,
     software: sys
   });
+})
+
+app.post("/api/shorturl/new", (req, res) => {
+  let longUrl = req.body.longUrl;
+  let key = Math.max(...Object.keys(shortened)) + 1;
+  shortened[key] = longUrl;
+  let resobj = {
+    original_url: longUrl,
+    short_url: key
+  }
+  dns.lookup(longUrl, (err, address) => {
+    if (err) resobj = {error: "invalid URL"};
+  })
+  res.json(resobj);
+})
+
+app.get("/api/shorturl/:short", (req, res) => {
+  let key = Number(req.params.short);
+  if (key) {
+    if (shortened[key]) res.redirect(shortened[key])
+    else res.json({error: "No short url found for given input"});
+  } else {
+    res.json({error: "shorturl must be a number"});
+  }
 })
 
 app.get("/", (req, res) => {
